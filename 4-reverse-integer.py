9bee59f4e25b6b64a8acefd63d3a5cbe87350366
Oleksandr Rozhko <sascha200116@gmail.com>
2026-01-19 14:40:14 +0200

    feat(4) : solution

---
+++ 4-reverse-integer.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverse(self, x):
+        if x >= 0:
+            x = int(str(x)[::-1])
+        else:
+            x = int("-" + str(x)[::-1].replace("-", ""))
+
+        if -(2**31) <= x <= 2**31 - 1:
+            return x
+        return 0
